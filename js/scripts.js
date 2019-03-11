604d025f22e80b81e66b504b6549ebcbcc8ffb60
michael beloff <michael.a.beloff@gmail.com>
2019-03-11 16:32:15 +1000

    nav tweaks

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 $(".nav-droplink").hover(function () {
-  $(".dropdown").show(100);
+  $(".dropdown").slideDown(100);
 })
 $(".nav-droplink").mouseleave(function () {
   $(".dropdown").hide(100);
@@ -10,8 +10,36 @@ $(".service-img-overlay").hover(function () {
   $(this).children(".overlay-heading").slideUp(200);
 })
 $(".service-img-overlay").mouseleave(function () {
-  $(this).children(".overlay-p").hide(100);
+  $(this).children(".overlay-p").slideUp(100);
   $(this).children(".overlay-heading").slideDown(200);
 })
 
+
+jQuery(function ($) {
+  $('a[href*="#"]:not([href="#"])').click(function (e) {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+      var target = $(this.hash);
+      headerHeight = 0;
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html,body').stop().animate({
+          scrollTop: target.offset().top - headerHeight
+        }, 750);
+      }
+    }
+  });
+});
+
+jQuery(document).ready(function ($) {
+  var hash = window.location.hash
+  if (hash == '' || hash == '#' || hash == undefined) return false;
+  var target = $(hash);
+  headerHeight = 0;
+  target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+  if (target.length) {
+    $('html,body').stop().animate({
+      scrollTop: target.offset().top - headerHeight
+    }, 750);
+  }
+});
 
