90faf4c0c0c68dca2408944c515641bc722e72a7
michael beloff <michael.a.beloff@gmail.com>
2019-03-11 12:56:34 +1000

    add map and some service content

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,17 @@
-$(".nav-droplink").mouseover(function () {
+$(".nav-droplink").hover(function () {
   $(".dropdown").show(100);
 })
 $(".nav-droplink").mouseleave(function () {
   $(".dropdown").hide(100);
 })
 
-$(".service-img-overlay").mouseover(function () {
-  $(this).children(".overlay-p").show(200);
+$(".service-img-overlay").hover(function () {
+  $(this).children(".overlay-p").slideDown(200);
+  $(this).children(".overlay-heading").slideUp(200);
 })
 $(".service-img-overlay").mouseleave(function () {
   $(this).children(".overlay-p").hide(100);
+  $(this).children(".overlay-heading").slideDown(200);
 })
 
 
