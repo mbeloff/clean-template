9110e24da05100dbdd8c757f8fae53c8c834b8f5
michael beloff <michael.a.beloff@gmail.com>
2019-03-26 11:18:33 +1000

    rcm test

---
+++ rcm/signRequest.php
@@ -0,0 +1,8 @@
+﻿<?php
+	$sharedSecret = "{sharedsecret}";
+	$post = file_get_contents('php://input');
+	$out=hash_hmac("sha256", $post, $sharedSecret);
+	
+	header('Content-Type: application/json');
+	echo '{"signature":"'.$out.'"}';
+?>
