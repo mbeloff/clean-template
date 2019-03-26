fbaae56f912f3be1e8c0f3f2198afbae9bc076dc
michael beloff <michael.a.beloff@gmail.com>
2019-03-26 11:25:14 +1000

    remove rcm test

--- rcm/signRequest.php
+++
@@ -1,8 +0,0 @@
-﻿<?php
-	$sharedSecret = "{sharedsecret}";
-	$post = file_get_contents('php://input');
-	$out=hash_hmac("sha256", $post, $sharedSecret);
-	
-	header('Content-Type: application/json');
-	echo '{"signature":"'.$out.'"}';
-?>
